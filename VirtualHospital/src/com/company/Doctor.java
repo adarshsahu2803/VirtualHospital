bb332c1b45a12b4ab102e7553d44e1f706354721
Hamna <hamnamtk@gmail.com>
2021-07-16 20:12:03 +0300

    Added doctor class

---
+++ VirtualHospital/src/com/company/Doctor.java
@@ -0,0 +1,75 @@
+
+public class Doctor {
+	
+	private String name;
+	private String department;
+	private int experience;
+	private double rating;
+	private int doctorID;
+	private time[] timeslots;
+	private String meetingLink;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public int getExperience() {
+		return experience;
+	}
+	
+	public double getRating() {
+		return rating;
+	}
+	
+	public int getDoctorID() {
+		return doctorID;
+	}
+	
+	public time[] getTimeslots() {
+		return timeslots;
+	}
+	
+	public String getMeetingLink() {
+		return meetingLink;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	public void setDoctorID(int doctorID) {
+		this.doctorID = doctorID;
+	}
+	
+	public void setTimeslots(time[] timeslots) {
+		this.timeslots = timeslots;
+	}
+	
+	public void setMeetingLink(String meetingLink) {
+		this.meetingLink = meetingLink;
+	}
+	
+	public String toString() {
+		return this.name + "\n" + "Experience : " + this.experience + "\nRating : " + this.rating; 
+	}
+
+}
