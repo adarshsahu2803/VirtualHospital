11d6398b24bf1f4eec4325c48ea4cbbdcb6fed7c
shalakasankar <86928446+shalakasankar@users.noreply.github.com>
2021-07-16 23:17:48 +0530

    Add files via upload

--- VirtualHospital/src/com/company/Patient.java
+++ VirtualHospital/src/com/company/Patient.java
@@ -1,15 +1,61 @@
-package com.company;
 
 public class Patient {
-    private String name, password;
-    private int age, userID;
-    private long aadharNum;
+	private String name,phone,aadhar;
+	private int age,ID,currentAppointments;
+	float height,weight;
+	boolean covid;
+	String password;
+public Patient(String name,int age,String aadhar,String phone,String password) {
+	this.name=name;
+	this.age=age;
+	this.aadhar=aadhar;
+	this.phone=phone;
+	currentAppointments=0;
+	this.password=password;
+	
+	
+}
+public void setHeight(float height) {
+	this.height=height;
+}
+public void setWeight(float weight) {
+	this.weight=weight;
+}
 
-    public void setUserID() {}
-    public void setName() {}
-    public void setAge() {}
-    public void setAadharNum() {}
-    public void setPassword() {}
+public void setCovid(boolean covid) {
+	this.covid= covid;
+}
 
+public String getName() {
+	return this.name;
+}
+public int getAge() {
+	return age;
+}
+public String getAadhar() {
+	return this.aadhar;
+}
+public String getPhone() {
+	return this.phone;
+}
+public float getHeight() {
+	return height;
+}
+public float getWeight() {
+	return weight;
+}
+public boolean getCovid() {
+	return covid;
+}
+public void setId(int ID) {
+	this.ID= ID;
+}
+public int getID() {
+	return ID;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
