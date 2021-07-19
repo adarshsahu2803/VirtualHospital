ecbde4c360ded32a0d246f4195e9366caae57ae4
Arjun Gowda A G <arjunag2001@gmail.com>
2021-07-19 21:48:55 +0530

    Added methods to Time

--- VirtualHospital/src/com/company/Time.java
+++ VirtualHospital/src/com/company/Time.java
@@ -1,11 +1,19 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Time {
     private String daySlot;
     public int[] timeSlots;
 
     public Time(String daySlot){
         this.daySlot = daySlot;
+    }
+
+    public String toString(){
+
+        return "";
     }
 
     public int[] setSlots(){
@@ -21,6 +29,26 @@ public class Time {
         }
         return Slots;
     }
+
+    public static String today(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d/MM/uuuu");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+    public static int Day_of_week (String day){
+        String[] dmy = day.split("/");
+        int d = Integer.parseInt(dmy[0]);
+        int m = Integer.parseInt(dmy[1]);
+        int y = Integer.parseInt(dmy[2]);
+        int y0,x,m0,d0;
+        y0=y-(14-m)/12;
+        x=y0+y0/4-y0/100+y0/400;
+        m0=m+12*((14-m)/12)-2;
+        d0=(d+x+31*m0/12)%7;
+        return d0+1;
+    }
+
+
 
 
 }
